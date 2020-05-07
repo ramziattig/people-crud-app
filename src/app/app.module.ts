fbe5fb0ac0751946f497a04f21b0f8854b601e70
ramzi attig <ramziattig@gmail.com>
2020-05-07 16:26:23 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PeoplelistComponent } from './peoplelist/peoplelist.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    PeoplelistComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
